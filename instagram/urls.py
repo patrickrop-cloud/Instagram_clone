d87d2b3800feab2b6ca5fccd667b4ba5945be6c2
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-17 20:16:42 +0300

    debug settings urls,views and models files

--- instagram/urls.py
+++ instagram/urls.py
@@ -8,16 +8,16 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
-    url(r'^uploadImage/$',views.uploadImage,name = 'uploadImage'),
+    path(r'^uploadImage/',views.uploadImage,name = 'uploadImage'),
     url(r'^registeruser/',views.registeruser,name='registeruser'),
     url(r'^login/',views.loginpage,name='loginpage'),
     url(r'^logout/$',views.logout,name='logout'),
     url(r'^profile/$',views.profile,name='profile'),
     url(r'^search/$', views.search_results, name='search_results'),
     url(r'^like/$', views.like, name='like'),
-    path(r'^photo/<str:pk>',views.viewPhoto,name='photo'),
-    path(r'^follow/<str:username>',views.follow,name='follow'),
-    path(r'post/<int:pk>/comment', views.post_detail, name='post_detail')
+    path(r'^photo/<str:pk>/',views.viewPhoto,name='photo'),
+    path(r'^follow/<str:username>/',views.follow,name='follow'),
+    path(r'post/<int:pk>/comment/', views.post_detail, name='post_detail')
     
 
 ]
