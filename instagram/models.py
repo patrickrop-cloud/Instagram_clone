f436338173856879d988337989e68649519bc149
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-17 14:44:50 +0300

    created class class profile to allow user upload a profile picture

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(default='default.jpg',upload_to='profile')
+    bio = models.TextField(max_length=300)
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
