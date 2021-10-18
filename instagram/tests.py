29e89e57ae3830c701ad761220ed18c8462fab94
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-18 11:49:17 +0300

    add test for profile tests

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
+
+from instagram.views import profile
 from .models import Image, Profile
 
 # Create your tests here.
 class ImageTestclass(TestCase):
     #setup method
     def setUp(self):
-        self.myimage=Image(image='image',name='Patrick',caption='Nice',likes=0,comments='nice pic')
+        self.myimage=Image(image='image',name='Patrick',caption='Nice',comments='nice pic')
 
      #Testing Instance
     def test_instance(self):
@@ -17,6 +19,12 @@ class ImageTestclass(TestCase):
         images=Image.objects.all()
         self.assertTrue(len(images)>0)
 
+    
+    def test_update_caption(self):
+        new_caption=Image.update_caption()
+        expected_caption=f'{new_caption}'
+        self.assertTrue(expected_caption,'new_image')
+
     #delete method
     def test_delete_images(self):
         self.myimage.save_image()
@@ -24,9 +32,16 @@ class ImageTestclass(TestCase):
         self.myimage.delete_image()
         self.assertTrue(len(images_record)==0)
 
-    def test_update_caption(self):
-        new_caption=Image.update_caption()
-        expected_caption=f'{new_caption}'
-        self.assertTrue(expected_caption,'new_image')
+class ProfileTestclass(TestCase):
+    #setUp method
+    def setUp(self):
+        self.profile=Profile(profile_pic='image',bio='Software developer')
+
+    #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    
+
 
     
