b42695bdc9645a0a7203e44afacaff9ee667eb48
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-18 16:59:29 +0300

    deploy

--- insta/settings.py
+++ insta/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_5$3c5+w_&b3lk=6o6*$lka@0*h-zy5b5+4&1ipiw!ltmmb1xe'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     '0.0.0.0'
     'the-yogify.herokuapp.com',
